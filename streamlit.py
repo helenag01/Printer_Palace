3eb2f9fff7a5849b62e32ad8e2bd2a30f5d4946f
mbertagna <78129017+mbertagna@users.noreply.github.com>
2022-11-09 16:19:51 -0800

    Update streamlit.py
    added search bar and value output

--- streamlit.py
+++ streamlit.py
@@ -4,4 +4,8 @@ st.write("HELLO WORLD")
 
 option = st.selectbox('How would you like to be contacted?', ('Email', 'Home phone', 'Mobile phone'))
 
+search = st.text_input("Query")
+
+st.write('You searched:', search)
+
 st.write('You selected:', option)
